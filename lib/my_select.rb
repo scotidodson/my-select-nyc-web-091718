1e5bb8db023140661703b105bba20bbe0ecd74e0
Scoti Dodson <scotidodson@gmail.com>
2018-09-06 01:40:51 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+  true_collection = []
+  collection.each do |i|
+      true_collection << i if collection.call(i) == true
+    end
+  true_collection
 end
+
+
+   # Returns an array containing all elements of enum for which the given block returns a true value.
+   # returns enumerator 
