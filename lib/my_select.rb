e6608394d3312a05523e466770e3f5eac9c9cc18
Scoti Dodson <scotidodson@gmail.com>
2018-09-06 01:45:22 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
-def my_select(collection)
- # your code here!
-end
+def my_select(collection) 
+  i = 0 
+  true_collection = [] 
+  while i < collection.length 
+    if (yield(collection[i]))
+      true_collection << collection[i] 
+    end 
+    i += 1 
+  end 
+  true_collection
+end 
+
+
+   # Returns an array containing all elements of enum for which the given block returns a true value.
+   # returns enumerator 
