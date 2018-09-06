2ddc633a581edaabc236b103be88ec80c5130fee
Scoti Dodson <scotidodson@gmail.com>
2018-09-06 01:43:39 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,15 @@
-def my_select(collection)
-  true_collection = []
-  collection.each do |i|
-      true_collection << yield(collection[i]) if collection(&block) == true
-    end
-  true_collection
-end
+def my_select(array) 
+  i =0 
+  select = [] 
+  while i < array.length 
+    if (yield(array[i]))
+      # select.push(array[i])OR 
+      select << array[i] 
+    end 
+    i += 1 
+  end 
+  select 
+end 
 
 
    # Returns an array containing all elements of enum for which the given block returns a true value.
